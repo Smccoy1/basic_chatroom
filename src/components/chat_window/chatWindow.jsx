d5532bc0a4cd50e5e305fcbb6cce474dda9c2259
Spencer McCoy <spencer.l.mccoy@gmail.com>
2021-11-11 14:16:54 -0700

    Created the main chat window and gave it some minor styling

---
+++ src/components/chat_window/chatWindow.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './chatWindow.css'
+import { Container } from 'react-bootstrap'
+
+function ChatWindow() {
+    return (
+        <div>
+            <Container>
+                <div className='chat-window'>
+                    <h3>This is the chat window</h3>
+                </div>
+            </Container>
+            
+        </div>
+    )
+}
+
+export default ChatWindow
