6c76461274ea29515e46ebb4806b20fbab511f78
Spencer McCoy <spencer.l.mccoy@gmail.com>
2021-11-17 15:35:29 -0700

    Added Chat Input to Chat Window

--- src/components/chat_window/chatWindow.jsx
+++ src/components/chat_window/chatWindow.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './chatWindow.css'
 import { Container } from 'react-bootstrap'
+import ChatInput from '../chatinput/chatInput'
 
 function ChatWindow() {
     return (
@@ -8,6 +9,7 @@ function ChatWindow() {
             <Container>
                 <div className='chat-window'>
                     <h3>This is the chat window</h3>
+                    <ChatInput />
                 </div>
             </Container>
             
